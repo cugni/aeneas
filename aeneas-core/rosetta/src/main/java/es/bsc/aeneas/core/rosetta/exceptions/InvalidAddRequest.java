0ded8f423b0968aa2185b64f7653b4f948318e6e
cugni <caronte12ebbro>
2014-01-31 11:14:29 +0100

    file moved

--- aeneas-core/rosetta/src/main/java/es/bsc/aeneas/core/rosetta/exceptions/InvalidAddRequest.java
+++
@@ -1,5 +0,0 @@
-package es.bsc.aeneas.core.rosetta.exceptions;
-
-public class InvalidAddRequest extends Exception {
-
-}
--- aeneas-core/rosetta/src/main/java/es/bsc/aeneas/core/rosetta/exceptions/PatternException.java
+++
@@ -1,9 +0,0 @@
-package es.bsc.aeneas.core.rosetta.exceptions;
-
-public class PatternException extends Exception {
-
-	public PatternException(String string) {
-		super(string);
-	}
-
-}
